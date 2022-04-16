e95664ddb859c759fa576eb88c64246779fe18d0
igorunderplayer <igorschug6@gmail.com>
2022-04-16 18:06:34 -0300

    embed test command

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -1,6 +1,6 @@
 use serenity::{
     client::Context,
-    model::channel::Message,
+    model::channel::Message
 };
 
 use serenity::framework::standard::{
@@ -12,7 +12,7 @@ use simple_process_stats::ProcessStats;
 
 
 #[group]
-#[commands(ping)]
+#[commands(ping, embed)]
 pub struct Info;
 
 #[command]
@@ -20,5 +20,15 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     let process_stats = ProcessStats::get().await.expect("could not get stats for running process");
     msg.reply(ctx, "pong").await?;
     msg.reply(ctx, format!("Memory usage {:?}MiB", process_stats.memory_usage_bytes / 1024 / 1024 )).await?;
+    Ok(())
+}
+
+#[command]
+async fn embed(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e|
+            e.title("EMBED PICA").description(":flushed: isso eh so uma embed de testes, n me estranhe")
+        )}).await?;
+
     Ok(())
 }
